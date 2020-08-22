599896d8e5b857114fa0464c748d3bc346c5d0c0
iDhruven <35227460+iDhruven@users.noreply.github.com>
2020-08-21 23:11:40 -0700

    Update Submit.py

--- Submit.py
+++ Submit.py
@@ -1,3 +1,6 @@
+import os
+
+r = \u001b[33m
 print ("------------The Submit Script Begins here------------------")
 
 SUBMIT_VERSION=2
@@ -7,6 +10,6 @@ print ("Submit script version:", SUBMIT_VERSION)
 print ("Debug Mode:", DEBUG_MODE)
 
 if DEBUG_MODE == 1:
-  print ("\u001b[33m DEBUG MODE ENABLED")
+  print 'r + "DEBUG MODE ENABLED"'
 
 print ("------------The Submit Script Ends here------------------")
