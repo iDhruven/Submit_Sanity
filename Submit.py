6befd8453d651c25b6be086a986b863c558add11
iDhruven <35227460+iDhruven@users.noreply.github.com>
2020-08-22 01:13:57 -0700

    Update Submit.py

--- Submit.py
+++ Submit.py
@@ -3,6 +3,7 @@ import os
 print ("-----------The Submit Script Begins here------------------")
 
 SUBMIT_VERSION=2
+DEBUG_MODE=$DEBUG_MODE
 
 print ("Submit script version:", SUBMIT_VERSION)
 print ("Debug Mode:", DEBUG_MODE)
