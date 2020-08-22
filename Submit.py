e82af4105da6c01203cc5a929bb9751ccabd9ac3
iDhruven <35227460+iDhruven@users.noreply.github.com>
2020-08-22 00:14:50 -0700

    Update Submit.py

--- Submit.py
+++ Submit.py
@@ -5,6 +5,9 @@ os.system("cls")
 r = "\u001b[33m"
 print ("------------The Submit Script Begins here------------------")
 
+print('\033[31m' + 'some red text')
+print('\033[39m') # and reset to default color
+
 SUBMIT_VERSION=2
 DEBUG_MODE=1
 
