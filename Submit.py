ecdc42f40667735f69fd821be6a1a181231592a9
iDhruven <35227460+iDhruven@users.noreply.github.com>
2020-08-22 12:06:52 -0700

    Update Submit.py

--- Submit.py
+++ Submit.py
@@ -1,4 +1,5 @@
 import os
+import git
 
 print ("-----------The Submit Script Begins here------------------")
 
@@ -17,7 +18,9 @@ SUBMIT_TO="GoldenGate Azul Hunter Archer"
 MA_PROJECT="Morphun_AzulAssets"
 GIT_REPO_URL="https://github.siri.apple.com/cslt/morphun.git"
 ARTIFACTORY_URL="https://artifactory.siri.apple.com"
-RELEASE_EMAIL_FROM=`git config user.email`
+RELEASE_EMAIL_FROM= repo.git.status()
 RELEASE_EMAIL_TO="morphun-discussion@group.apple.com"
+
+print ("EMAIL:", RELEASE_EMAIL_FROM)
 
 print ("-----------The Submit Script Ends here------------------")
