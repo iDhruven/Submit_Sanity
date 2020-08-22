9b580fd92351b010e27a60ecd2fbdaad66ca11ee
iDhruven <35227460+iDhruven@users.noreply.github.com>
2020-08-22 12:09:22 -0700

    Update Submit.py

--- Submit.py
+++ Submit.py
@@ -18,7 +18,7 @@ SUBMIT_TO="GoldenGate Azul Hunter Archer"
 MA_PROJECT="Morphun_AzulAssets"
 GIT_REPO_URL="https://github.siri.apple.com/cslt/morphun.git"
 ARTIFACTORY_URL="https://artifactory.siri.apple.com"
-RELEASE_EMAIL_FROM= repo.git.status()
+RELEASE_EMAIL_FROM= os.system('git --version')
 RELEASE_EMAIL_TO="morphun-discussion@group.apple.com"
 
 print ("EMAIL:", RELEASE_EMAIL_FROM)
