b723fb27a1816483211d79bc756fac870079a8f1
iDhruven <35227460+iDhruven@users.noreply.github.com>
2020-08-22 14:02:44 -0700

    Update Submit.py

--- Submit.py
+++ Submit.py
@@ -72,7 +72,7 @@ CHOICE=[os.environ['CHOICE']]
 print CHOICE
 
 HELP = "H"
-result = [i for i in HELP if i in CHOICE]
+result = [i for i in CHOICE if i in HELP]
 print str(result)
 
 if CHOICE == "H" or "H,A" or "H,A,B" or "H,A,B,I" or "H,A,B,I,T": 
