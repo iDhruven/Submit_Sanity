f42418656330b069060057f5e0be33e8b37d72a3
iDhruven <35227460+iDhruven@users.noreply.github.com>
2020-08-21 23:00:38 -0700

    Update Submit.py

--- Submit.py
+++ Submit.py
@@ -6,4 +6,7 @@ DEBUG_MODE=1
 print ("Submit script version:", SUBMIT_VERSION)
 print ("Debug Mode:", DEBUG_MODE)
 
+if DEBUG_MODE == 1:
+  print ("DEBUG MODE ENABLED")
+
 print ("------------The Submit Script Ends here------------------")
