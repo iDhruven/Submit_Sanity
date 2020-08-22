1eec8e02cbc56441bfdd6b6b2d753901984df7c6
iDhruven <35227460+iDhruven@users.noreply.github.com>
2020-08-22 00:38:29 -0700

    Update Submit.py

--- Submit.py
+++ Submit.py
@@ -1,11 +1,11 @@
-r = "\u001b[33m"
+coloredlogs.install(isatty=True)
 print ("------------The Submit Script Begins here------------------")
 
 import os
 os.system('echo "Hello World!"')
 
-print('\033[31m' + 'some red text')
-print('\033[39m') # and reset to default color
+#print('\033[31m' + 'some red text')
+#print('\033[39m') # and reset to default color
 
 SUBMIT_VERSION=2
 DEBUG_MODE=1
@@ -14,6 +14,6 @@ print ("Submit script version:", SUBMIT_VERSION)
 print ("Debug Mode:", DEBUG_MODE)
 
 if DEBUG_MODE == 1:
-  print 'r + "DEBUG MODE ENABLED"'
+  print ("DEBUG MODE ENABLED")
 
 print ("------------The Submit Script Ends here------------------")
