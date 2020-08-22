d2371da718a2849866f85907ad0bf30e4713de69
iDhruven <35227460+iDhruven@users.noreply.github.com>
2020-08-22 01:03:29 -0700

    Update Submit.py

--- Submit.py
+++ Submit.py
@@ -1,11 +1,6 @@
 import os
 
 os.system('printf "\e[31m------------The Submit Script Begins here------------------\e[0m\n"')
-#print ("------------The Submit Script Begins here------------------")
-
-
-os.system('echo "Hello World!"')
-os.system('printf "\\e[31mRED COLOR\\e[0m\\n"')
 
 #print('\033[31m' + 'some red text')
 #print('\033[39m') # and reset to default color
@@ -19,5 +14,4 @@ print ("Debug Mode:", DEBUG_MODE)
 if DEBUG_MODE == 1:
   print ("DEBUG MODE ENABLED")
 
-#print ("------------The Submit Script Ends here------------------")
 os.system('printf "\\e[31m------------The Submit Script Begins here------------------\\e[0m\\n"')
