89e8e736eb8e33fc716fb83c5a119dcb6c1d8525
iDhruven <35227460+iDhruven@users.noreply.github.com>
2020-08-22 12:35:49 -0700

    Update Submit.py

--- Submit.py
+++ Submit.py
@@ -31,10 +31,43 @@ SUBMIT_IGNORE=("gradle/wrapper/gradle-wrapper.jar")
 # Reset in case getopts has been used previously
 OPTIND=1
 
-# Text colors
-GREEN_TEXT=os.environ['(tput setaf 2)']
-RED_TEXT=os.environ['(tput setaf 1)']
-CLEAR_TEXT=os.environ['(tput sgr0)']
+# getopts
+RESUBMIT=0
+FULL_SUBMIT=1
+PROJECT_SUBMIT=0
+INIT_SUBMIT=0
+TAG_SUBMIT=0
+ARTIFACTORY_SUBMIT=0
+MOBILE_ASSETS_SUBMIT=0
+PYTHON_MORPHUN_SUBMIT=0
+RELEASE_EMAIL_SUBMIT=0
+
+# print help
+def print_help():
+    print ("")
+    print "Using the script without any options will result in a full submission of : ", PROJECT
+    print ("")
+    print ("Any combination of Major, Minor version is allowed. This allows for future submissions as well as resubmissions.")
+    print ("")
+    print ("Example usage:")
+    print ("Full submission: ./submit or ./submit -a")
+    print "Submit ", PROJECT, "to B&I only: ./submit -b"
+    print ("Submit MobileAssets and artifactory package: ./submit -mp")
+    print ("")
+    print ("Using any combination of options is allowed.")
+    print ("")
+    print ("  -a : full submission")
+    print ("  -i : initialize submission")
+    print "  -b : submit ", PROJECT, "to B&I"
+    print ("  -t : tag submission and push to github")
+    print ("  -p : submit macOS artifactory package")
+    print ("  -m : submit MobileAssets")
+    print ("  -l : submit python Morphun")
+    print ("  -e : send release email")
+    print ("  -h,? : this help message")
+    print ("")
+
+print_help()
 
 
 
