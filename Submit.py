e04237990d7e0f26f0d3f26affbf58d371632af7
iDhruven <35227460+iDhruven@users.noreply.github.com>
2020-08-21 22:25:46 -0700

    Update Submit.py

--- Submit.py
+++ Submit.py
@@ -1,4 +1,8 @@
 print ("------------The Submit Script Begins here------------------")
 
+SUBMIT_VERSION=2
+DEBUG_MODE=1
+
+print ("Submit script version $SUBMIT_VERSION")
 
 print ("------------The Submit Script Ends here------------------")
