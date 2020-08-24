900bc3d87f43c2638fe918b99d1a896bda56fc51
iDhruven <35227460+iDhruven@users.noreply.github.com>
2020-08-24 00:16:17 -0700

    Update SubmitLogo.py

--- Submit/SubmitLogo.py
+++ Submit/SubmitLogo.py
@@ -21,13 +21,13 @@ def print_logo():
   logo2 = pyfiglet.figlet_format("Morphun", font = "starwars")
   logo3 = pyfiglet.figlet_format("Morphun", font = "stampatello")
   logo4 = pyfiglet.figlet_format("Morphun", font = "rozzo")
-  logo5 = pyfiglet.figlet_format("Morphun", font = "roman")
-  logo6 = pyfiglet.figlet_format("Morphun", font = "sblood")
-  logo7 = pyfiglet.figlet_format("Morphun", font = "nvscript")
+  #logo5 = pyfiglet.figlet_format("Morphun", font = "roman")
+  #logo6 = pyfiglet.figlet_format("Morphun", font = "sblood")
+  #logo7 = pyfiglet.figlet_format("Morphun", font = "nvscript")
   logo8 = pyfiglet.figlet_format("Morphun", font = "doh")
-  logo9 = pyfiglet.figlet_format("Morphun", font = "cosmic")
-  logo10 = pyfiglet.figlet_format("Morphun", font = "broadway")
-  logo11 = pyfiglet.figlet_format("Morphun", font = "banner3-D")
+  #logo9 = pyfiglet.figlet_format("Morphun", font = "cosmic")
+  #logo10 = pyfiglet.figlet_format("Morphun", font = "broadway")
+  #logo11 = pyfiglet.figlet_format("Morphun", font = "banner3-D")
 
   
   
@@ -36,12 +36,12 @@ def print_logo():
   print(logo2)
   print(logo3)
   print(logo4)
-  print(logo5)
-  print(logo6)
-  print(logo7)
+  #print(logo5)
+  #print(logo6)
+  #print(logo7)
   print(logo8)
-  print(logo9)
-  print(logo10)
-  print(logo11)
+  #print(logo9)
+  #print(logo10)
+  #print(logo11)
   
   
