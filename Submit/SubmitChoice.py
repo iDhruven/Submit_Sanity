acaacf20009eadaf6bdb407f3a0ce9f9dd1f9aa5
iDhruven <35227460+iDhruven@users.noreply.github.com>
2020-08-23 22:25:35 -0700

    Create SubmitChoice.py

---
+++ Submit/SubmitChoice.py
@@ -0,0 +1,25 @@
+#ChoiceSelection
+
+CHOICE=[os.environ['CHOICE']] 
+print "You have selected :", CHOICE
+
+H = 'H'
+A = 'A'
+B = 'B'
+E = 'E'
+I = 'I'
+L = 'L'
+M = 'M'
+P = 'P'
+T = 'T'
+R = 'R'
+H = [i for i in CHOICE if H in i]
+A = [i for i in CHOICE if A in i]
+B = [i for i in CHOICE if B in i]
+E = [i for i in CHOICE if E in i]
+I = [i for i in CHOICE if I in i]
+L = [i for i in CHOICE if L in i]
+M = [i for i in CHOICE if M in i]
+P = [i for i in CHOICE if P in i]
+T = [i for i in CHOICE if T in i]
+R = [i for i in CHOICE if R in i]
