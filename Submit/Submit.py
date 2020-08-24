dedd4978cead2f37b241eeea3467339097688920
iDhruven <35227460+iDhruven@users.noreply.github.com>
2020-08-23 21:55:29 -0700

    Update Submit.py

--- Submit/Submit.py
+++ Submit/Submit.py
@@ -15,19 +15,6 @@ print "Debug Mode:", DEBUG_MODE
 if DEBUG_MODE == 1:
   print ("DEBUG MODE ENABLED")
   
-# Project settings
-PROJECT="Morphun"
-SUBMIT_TO="GoldenGate Azul Hunter Archer"
-MA_PROJECT="Morphun_AzulAssets"
-GIT_REPO_URL="https://github.siri.apple.com/cslt/morphun.git"
-ARTIFACTORY_URL="https://artifactory.siri.apple.com"
-
-RELEASE_EMAIL_FROM= os.system('git config user.email')
-
-RELEASE_EMAIL_TO="morphun-discussion@group.apple.com"
-
-print "EMAIL:", RELEASE_EMAIL_FROM
-
 # Files 
 GIT_IGNORE=".gitignore"
 SUBMIT_IGNORE=("gradle/wrapper/gradle-wrapper.jar")
