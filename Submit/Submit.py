e6109ce6b51b22f400600edef4b1c466adfa5270
iDhruven <35227460+iDhruven@users.noreply.github.com>
2020-08-23 21:41:37 -0700

    Update Submit.py

--- Submit/Submit.py
+++ Submit/Submit.py
@@ -2,6 +2,8 @@ import os
 import turtle
 import pyfiglet 
 
+import Submit_Help
+
 print ("------------------------------The Submit Script Begins here--------------------------------------")
 
 SUBMIT_VERSION=2
@@ -44,33 +46,6 @@ MOBILE_ASSETS_SUBMIT=0
 PYTHON_MORPHUN_SUBMIT=0
 RELEASE_EMAIL_SUBMIT=0
 
-# print help
-def print_help():
-    print ("---------------Help Manual------------")
-    print ("")
-    print  "Using the script without any options will result in a full submission of : ", PROJECT
-    print ("")
-    print ("Any combination of Major, Minor version is allowed. This allows for future submissions as well as resubmissions.")
-    print ("")
-    print ("Example usage:")
-    print ("Full submission: ./submit or ./submit -a")
-    print  "Submit ", PROJECT, "to B&I only: ./submit -b"
-    print ("Submit MobileAssets and artifactory package: ./submit -mp")
-    print ("")
-    print ("Using any combination of options is allowed.")
-    print ("")
-    print ("  -a : full submission")
-    print ("  -i : initialize submission")
-    print  "  -b : submit ", PROJECT, "to B&I"
-    print ("  -t : tag submission and push to github")
-    print ("  -p : submit macOS artifactory package")
-    print ("  -m : submit MobileAssets")
-    print ("  -l : submit python Morphun")
-    print ("  -e : send release email")
-    print ("  -h,? : this help message")
-    print ("")
-
-    
 CHOICE=[os.environ['CHOICE']] 
 print "You have selected :", CHOICE
 
