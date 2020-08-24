d1955908ccb6990d0d2524cb9b206ebeafdb3089
iDhruven <35227460+iDhruven@users.noreply.github.com>
2020-08-24 00:12:50 -0700

    Update SubmitLogo.py

--- Submit/SubmitLogo.py
+++ Submit/SubmitLogo.py
@@ -16,9 +16,32 @@ def print_logo():
   print("| |    |      |    | /             ")
   print(" \|____|      |____|               ")
 
-  logo1 = pyfiglet.figlet_format("Morphun", font = "starwars"  )
-  logo2 = pyfiglet.figlet_format("Morphun", font = "starwars"  )
-  logo = pyfiglet.figlet_format("Morphun", font = "poison"  ) 
+  logo  = pyfiglet.figlet_format("Morphun", font = "poison")
+  logo1 = pyfiglet.figlet_format("Morphun", font = "stop")
+  logo2 = pyfiglet.figlet_format("Morphun", font = "starwars")
+  logo3 = pyfiglet.figlet_format("Morphun", font = "stampatello")
+  logo4 = pyfiglet.figlet_format("Morphun", font = "rozzo")
+  logo5 = pyfiglet.figlet_format("Morphun", font = "roman")
+  logo6 = pyfiglet.figlet_format("Morphun", font = "sblood")
+  logo7 = pyfiglet.figlet_format("Morphun", font = "nvscript")
+  logo8 = pyfiglet.figlet_format("Morphun", font = "doh")
+  logo9 = pyfiglet.figlet_format("Morphun", font = "cosmic")
+  logo10 = pyfiglet.figlet_format("Morphun", font = "broadway")
+  logo11 = pyfiglet.figlet_format("Morphun", font = " banner3-D")
+
+  
+  
   print(logo)
   print(logo1)
   print(logo2)
+  print(logo3)
+  print(logo4)
+  print(logo5)
+  print(logo6)
+  print(logo7)
+  print(logo8)
+  print(logo9)
+  print(logo10)
+  print(logo11)
+  
+  
