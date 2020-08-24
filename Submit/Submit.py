c75d7b7a37000c06c2d0459d0f8764d3db3e82a4
iDhruven <35227460+iDhruven@users.noreply.github.com>
2020-08-23 23:38:33 -0700

    Update Submit.py

--- Submit/Submit.py
+++ Submit/Submit.py
@@ -4,7 +4,8 @@ import pyfiglet
 
 import SubmitHelp
 import SubmitLogo
-import SubmitProjectSettings
+
+from SubmitProjectSettings import *
 
 print ("------------------------------The Submit Script Begins here--------------------------------------")
 
@@ -66,7 +67,7 @@ if CHOICE == A:
   print ("Full Submission Script comes here!")
   FULL_SUBMIT=1
 if CHOICE == B:
-  print "Submit ", PROJECT ,"to B&I Script comes here!"
+  print "Submit ", SubmitProjectSettings.PROJECT ,"to B&I Script comes here!"
   PROJECT_SUBMIT=1
   FULL_SUBMIT=0
 if CHOICE == E:
