27a71bbddcc0bf034d9af47ff75c0378a7a3a142
iDhruven <35227460+iDhruven@users.noreply.github.com>
2020-09-02 20:50:28 -0700

    Update SubmitTag.py

--- Submit/SubmitTag.py
+++ Submit/SubmitTag.py
@@ -2,7 +2,7 @@ import os
 
 from SubmitProjectSettings import *
 
-VERSION=[os.environ['VERSION']] 
+VERSION=[os.environ('VERSION')] 
 print ("TAG HERE ---->", VERSION)
 print (PROJECT)
 os.system ('git tag -l | grep "$PROJECT-"')
