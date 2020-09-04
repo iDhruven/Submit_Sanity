968cad7abac789aca49224e110d5f643f205ddbf
iDhruven <35227460+iDhruven@users.noreply.github.com>
2020-09-04 15:56:45 -0700

    Create SubmitClean.py

---
+++ Submit/SubmitClean.py
@@ -0,0 +1,14 @@
+import os
+
+print ("Cleaning Project...")
+os.system('make clean > /dev/null')
+
+# Clean MobileAssets
+print ("Cleaning Mobile Assets...")
+#os.system('cd MobileAssets')
+os.system('cd MobileAssets | ./PopulateAssets.sh clean > /dev/null')
+os.system('cd ..')
+
+# Clean Python Morphun
+print ("Cleaning Python Morphun...")
+os.system('make -C interface/python clean > /dev/null')
