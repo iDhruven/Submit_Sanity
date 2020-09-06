5bfebbd54a6022baeb825a70a54f986db7798bf2
iDhruven <35227460+iDhruven@users.noreply.github.com>
2020-09-06 16:23:38 -0700

    Update SubmitMajorTag.py

--- Submit/SubmitMajorTag.py
+++ Submit/SubmitMajorTag.py
@@ -4,7 +4,7 @@ from SubmitProjectSettings import *
 
 VERSION=os.environ['VERSION'] 
 New_Major_Version = int(VERSION)
-print ("The New Version is ----->" ,(New_Minor_Version + 0.1))
+print ("The New Version is ----->" ,(New_Major_Version + 1))
 #print ("TAG HERE ---->", VERSION)
 #print (PROJECT)
 #os.system ('git tag -l | grep "Morphun-" | sort -n | tail -1')
