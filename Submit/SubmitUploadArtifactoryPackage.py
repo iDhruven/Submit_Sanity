d1ca9201a286d549fa46f6ac21384c796c7469e2
iDhruven <35227460+iDhruven@users.noreply.github.com>
2020-09-06 16:37:46 -0700

    Update SubmitUploadArtifactoryPackage.py

--- Submit/SubmitUploadArtifactoryPackage.py
+++ Submit/SubmitUploadArtifactoryPackage.py
@@ -2,3 +2,6 @@ from SubmitProjectSettings.py import *
 
 ARTIFACTORY_PATH=ARTIFACTORY_URL/libs-release-local/com/apple/csl/morphun/(New_Version)/
 TGZ_NAME=morphun-(New_Version)-mac.tgz
+
+print(ARTIFACTORY_PATH)
+print(TGZ_NAME)
