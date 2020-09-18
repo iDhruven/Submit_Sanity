68de8d5a50c6a49300ae0da27f47e1b2581c36de
iDhruven <35227460+iDhruven@users.noreply.github.com>
2020-09-17 23:24:08 -0700

    Update SubmitMinorTag.py

--- Submit/SubmitMinorTag.py
+++ Submit/SubmitMinorTag.py
@@ -2,9 +2,6 @@ import os
 
 from SubmitProjectSettings import *
 
-VERSION=os.environ['VERSION'] 
-New_Version = int(VERSION)
-print ("The New Version is ----->" ,(New_Version + 0.1))
-#print ("TAG HERE ---->", VERSION)
-#print (PROJECT)
-#os.system ('git tag -l | grep "Morphun-" | sort -n | tail -1')
+#VERSION=os.environ['VERSION'] 
+#New_Version = int(VERSION)
+#print ("The New Version is ----->" ,(New_Version + 0.1))
