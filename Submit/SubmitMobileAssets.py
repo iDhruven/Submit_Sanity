02c8e60b1dbc1265a6a54f1381fec807059d1417
iDhruven <35227460+iDhruven@users.noreply.github.com>
2020-09-17 22:26:15 -0700

    Update SubmitMobileAssets.py

--- Submit/SubmitMobileAssets.py
+++ Submit/SubmitMobileAssets.py
@@ -6,7 +6,7 @@ from SubmitMajorTag import *
 print ("Building MobileAssets...")
 os.system('cd MobileAssets | ./PopulateAssets.sh clean | ./PopulateAssets.sh macos')
 
-FQ_MAPROJECT = MA_PROJECT + "-" + str(New_Version)
+FQ_MAPROJECT = MA_PROJECT + "-" + str(NEW_VERSION)
 print(FQ_MAPROJECT)
 
 print ("Submitting " + FQ_MAPROJECT + " to Speedwagon... (5 min, alert on completion)")
