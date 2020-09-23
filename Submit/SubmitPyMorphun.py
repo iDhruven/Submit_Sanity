1483e88a80b0be10db84da6e82dc516b81a211cd
iDhruven <35227460+iDhruven@users.noreply.github.com>
2020-09-23 15:50:23 -0700

    Create SubmitPyMorphun.py

---
+++ Submit/SubmitPyMorphun.py
@@ -0,0 +1,7 @@
+import os
+
+print ("-------------------------Submit PyMorphun Starts here! ------------------------------")
+
+os.system('make -C interface/python publish-macos')
+
+print ("-------------------------Submit PyMorphun Starts here! ------------------------------")
