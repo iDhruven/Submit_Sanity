fd397db8cb8b8a277269b42bd499d7756b1995ee
iDhruven <35227460+iDhruven@users.noreply.github.com>
2020-09-23 15:48:12 -0700

    Update SubmitMinorTag.py

--- Submit/SubmitMinorTag.py
+++ Submit/SubmitMinorTag.py
@@ -2,6 +2,11 @@ import os
 
 from SubmitProjectSettings import *
 
-#VERSION=os.environ['VERSION'] 
-#New_Version = int(VERSION)
-#print ("The New Version is ----->" ,(New_Version + 0.1))
+VERSION=os.environ['VERSION'] 
+Float_Version = float(VERSION)
+
+NEW_VERSION = Float_Version + 0.1
+
+print ("#The New Version is ----->" ,(NEW_VERSION))
+print ("NEW_VERSION =", NEW_VERSION)
+
