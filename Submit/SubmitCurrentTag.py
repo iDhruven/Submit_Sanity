00ed5dea29dd3c201fb245fd5c431ae5496c39cf
iDhruven <35227460+iDhruven@users.noreply.github.com>
2020-09-23 15:46:27 -0700

    Create SubmitCurrentTag.py

---
+++ Submit/SubmitCurrentTag.py
@@ -0,0 +1,11 @@
+import os
+
+from SubmitProjectSettings import *
+
+VERSION=os.environ['VERSION'] 
+
+NEW_VERSION = float(VERSION)
+
+print ("#The New Version is ----->" ,(NEW_VERSION))
+print ("NEW_VERSION =", NEW_VERSION)
+
